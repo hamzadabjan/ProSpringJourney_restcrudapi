d4339b093d0a68ae05080b88a094706861e04bbe
Hamza <hamza.dabjan@gmail.com>
2024-09-06 15:14:08 +0300

    PostConstruct and PathVariable

--- src/main/java/com/hamza/springboot/training/restcrudapis/rest/StudentRestController.java
+++ src/main/java/com/hamza/springboot/training/restcrudapis/rest/StudentRestController.java
@@ -2,7 +2,9 @@ package com.hamza.springboot.training.restcrudapis.rest;
 
 
 import com.hamza.springboot.training.restcrudapis.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +15,27 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    // define endpoint for "/student" - return a list of student
+    List<Student> theStudents = new ArrayList<>();
 
-    @GetMapping("/students")
-    public List<Student> getStudent(){
-        List<Student> theStudents = new ArrayList<>();
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Hamza","Dabjan"));
         theStudents.add(new Student("Hanan","Shmiea"));
         theStudents.add(new Student("Rayhan","Dabjan"));
+    }
+
+    // define endpoint for "/student" - return a list of student
+    @GetMapping("/students")
+    public List<Student> getStudent(){
 
         return theStudents;
+    }
+
+    @GetMapping("/students/{studentId}")
+
+    public Student getSingleStudent(@PathVariable int studentId){
+        return theStudents.get(studentId);
     }
 }
