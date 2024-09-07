52bd3154b269d333ccfaec58a65c754924485766
Hamza <hamza.dabjan@gmail.com>
2024-09-07 21:53:33 +0300

    REST Exception Handling

--- src/main/java/com/hamza/springboot/training/restcrudapis/rest/StudentRestController.java
+++ src/main/java/com/hamza/springboot/training/restcrudapis/rest/StudentRestController.java
@@ -52,6 +52,7 @@ public class StudentRestController {
         //create a StudentErrorResponse
 
         StudentErrorResponse error = new StudentErrorResponse();
+
         error.setStatus(HttpStatus.NOT_FOUND.value());
         error.setMessage(exc.getMessage());
         error.setTimeStamp(System.currentTimeMillis());
@@ -59,5 +60,21 @@ public class StudentRestController {
         // return ResponseEntity
 
         return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity <StudentErrorResponse> handelException(Exception exc){
+
+        //create a StudentErrorResponse
+
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
     }
 }
