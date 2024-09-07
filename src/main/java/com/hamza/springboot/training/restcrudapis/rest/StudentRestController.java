a044c267663bc88b6804c8516689494452802d8c
Hamza <hamza.dabjan@gmail.com>
2024-09-07 20:59:38 +0300

    REST Exception Handling

--- src/main/java/com/hamza/springboot/training/restcrudapis/rest/StudentRestController.java
+++ src/main/java/com/hamza/springboot/training/restcrudapis/rest/StudentRestController.java
@@ -3,10 +3,9 @@ package com.hamza.springboot.training.restcrudapis.rest;
 
 import com.hamza.springboot.training.restcrudapis.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.AbstractList;
 import java.util.ArrayList;
@@ -36,6 +35,29 @@ public class StudentRestController {
     @GetMapping("/students/{studentId}")
 
     public Student getSingleStudent(@PathVariable int studentId){
+
+
+        if((studentId >= theStudents.size()) || (studentId<0)){
+            throw new StudentNotFoundException("student id not found - " + studentId);
+        }
+
         return theStudents.get(studentId);
+    }
+
+    // Add an exception handler using @ExceptionHandler
+
+    @ExceptionHandler
+    public ResponseEntity <StudentErrorResponse> handelException(StudentNotFoundException exc){
+
+        //create a StudentErrorResponse
+
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+
+        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
     }
 }
